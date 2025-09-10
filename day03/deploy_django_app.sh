1a4801b1bf9d1f21cadae15c1839fe1812a52f10
Yashwardhan Shakya <yashwardhan0703@gmail.com>
2025-09-10 09:29:14 +0000

    initial commit

--- day03/deploy_django_app.sh
+++ day03/deploy_django_app.sh
@@ -15,4 +15,41 @@ install_requirements() {
 }
 
 required_restarts() {
-	sudo
+	sudo systemctl enable docker
+	sudo systemctl enable nginx
+}
+
+required_restarts() {
+        sudo chown $USER /var/run/docker.sock
+        sudo systemctl enable docker
+        sudo systemctl enable nginx
+	sudo systemctl restart docker
+}
+
+deploy() {
+         docker build -t notes-app .
+         docker run -d -p 8000:8000 notes-app:latest
+}
+
+echo "******** DEPLOYMENT STARTED ********"
+if ! code_clone; then
+        echo "the code directory already exists"
+        cd django-notes-app
+fi
+
+
+if ! install_requirements; then
+        echo "Installation failed"
+        exit 1
+fi
+
+
+if ! required_restarts; then
+        echo "System fault identified"
+        exit 1
+fi
+
+deploy
+
+echo "******** DEPLOYMENT DONE ********"
+                                                  49,10         Bot
